f4ef696c46264f6bc9da9567398df6cccb3ce7ad
Ashutosh <ashutoshmohanty13703@gmail.com>
2025-02-28 19:38:09 +0530

    test

--- src/lib/firestore/firebase_admin.jsx
+++ src/lib/firestore/firebase_admin.jsx
@@ -7,7 +7,7 @@ if (admin.apps.length === 0) {
       credential: admin.credential.cert({
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+        privateKey: process.env.FIREBASE_PRIVATE_KEY
       })
     });
     console.log("Firebase Admin initialized successfully");
