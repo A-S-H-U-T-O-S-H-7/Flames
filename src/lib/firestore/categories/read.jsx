1645c80a935a241962404876f0fd0785c752d897
Ashutosh <ashutoshmohanty13703@gmail.com>
2025-03-13 21:17:33 +0530

    cate

--- src/lib/firestore/categories/read.jsx
+++ src/lib/firestore/categories/read.jsx
@@ -1,43 +1,28 @@
 "use client";
 
 import { db } from "../firebase";
-import { collection, query, limit, startAfter, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore";
 import useSWRSubscription from "swr/subscription";
 
-export function useCategories({ pageLimit, lastSnapDoc }= {}) {
+export function useCategories() {
   const { data, error } = useSWRSubscription(
-    ["categories", pageLimit, lastSnapDoc],
-    ([path, pageLimit, lastSnapDoc], { next }) => {
+    ["categories"],
+    ([path], { next }) => {
       const ref = collection(db, path);
-      let q = query(ref, limit(pageLimit ?? 10));
-
-      if (lastSnapDoc) {
-        q = query(q, startAfter(lastSnapDoc));
-      }
-
       const unsub = onSnapshot(
-        q,
+        ref,
         (snapshot) =>
-          next(null, {
-            list:
-              snapshot.docs.length === 0
-                ? null
-                : snapshot.docs.map((snap) => snap.data()),
-            lastSnapDoc:
-              snapshot.docs.length === 0
-                ? null
-                : snapshot.docs[snapshot.docs.length - 1],
-          }),
+          next(
+            null,
+            snapshot.docs.length === 0
+              ? null
+              : snapshot.docs.map((snap) => snap.data())
+          ),
         (err) => next(err, null)
       );
       return () => unsub();
     }
   );
 
-  return {
-    data: data?.list,
-    lastSnapDoc: data?.lastSnapDoc,
-    error: error?.message,
-    isLoading: data === undefined,
-  };
+  return { data, error: error?.message, isLoading: data === undefined };
 }
