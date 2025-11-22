56afbbcdec4636b3e5cf1440a9dd0022674a1193
Ashutosh Mohanty <ashutoshmohanty13703@gmail.com>
2025-11-22 08:00:15 +0530

    try1

--- src/app/(Web)/page.js
+++ src/app/(Web)/page.js
@@ -3,8 +3,10 @@ import React from 'react'
 import GiftsSection from '@/components/web/home/HomeGiftSection'
 import InstaBanner from '@/components/web/home/InstaBanner'
 import CustomerReviews from '@/components/web/home/CustomerReview'
+
 import CategoryClientWrapper from '@/components/clientCode/CategoryClientWrapper'
 import BannersClientWrapper from '@/components/clientCode/BannersClientWrapper'
+
 import FaqsClientWrapper from '@/components/clientCode/FaqsClientWrapper'
 import ShowcasedCollectionsClientWrapper from '@/components/clientCode/ShowcasedCollectionsClientWrapper'
 import FeaturedClientWrapper from '@/components/clientCode/FeaturedClientWrapper'
